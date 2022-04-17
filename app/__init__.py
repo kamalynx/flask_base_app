37d8efe9c4afdbae5037bae2b60cd53dced8783f
kamalynx <kamalynxpro@gmail.com>
2022-04-17 18:35:57 +0500

    enabled logs to file located at instance path

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,21 @@
 """Initialize application."""
+import logging
+import os
+
 from flask import Flask
+from werkzeug.utils import safe_join
 
 
 def create_app():
     """Create an application's instance."""
     app = Flask(__name__)
+
+    if not os.path.exists(app.instance_path):
+        os.mkdir(app.instance_path)
+
+    if not app.debug:
+        log_file = safe_join(app.instance_path, '{0}.log'.format(__name__))
+        logging.basicConfig(filename=log_file, level=logging.DEBUG)
 
     with app.app_context():
         try:
